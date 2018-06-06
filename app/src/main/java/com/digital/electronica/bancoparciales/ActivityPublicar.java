257b7643643d181f1ad9ba5bab237b272bd30952
laurasgm <laurasgm@github>
2018-06-05 19:08:26 -0500

    Firebase upload listo con pre visualizacion de la imagen

--- app/src/main/java/com/digital/electronica/bancoparciales/ActivityPublicar.java
+++ app/src/main/java/com/digital/electronica/bancoparciales/ActivityPublicar.java
@@ -1,12 +1,75 @@
 package com.digital.electronica.bancoparciales;
 
+import android.annotation.TargetApi;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission_group.CAMERA;
+
 
 public class ActivityPublicar extends AppCompatActivity {
+
+    LinearLayout relative;
+
+
+    private static String APP_DIRECTORY = "MyPictureApp/";
+    private static String MEDIA_DIRECTORY = APP_DIRECTORY + "PictureApp";
+
+    private final int RespuestaCamara = 100;
+    private final int RespuestaIMAGEN = 200;
+    private final int MY_PERMISSIONS = 100;
+
+    private StorageReference mStorageRef;
+
+    Long timestamp = System.currentTimeMillis() / 1000;
+    String imageName = timestamp.toString() + ".jpg";
+
+    private String mPath;
+
+    String Directorio;
+    Context prueba = this;
+
+    Button agregar;
+    LinearLayout layout;
+    ImageView image;
+
+    private ProgressDialog mProgressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +81,102 @@ public class ActivityPublicar extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setTitle("Agregar Parcial");
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        agregar = (Button) findViewById(R.id.btnagregar);
+        image = (ImageView) findViewById(R.id.image23);
+        layout = (LinearLayout) findViewById(R.id.layout);
+
+        mProgressDialog = new ProgressDialog(this);
+
+
+        if (mayRequestStoragePermission())
+            agregar.setEnabled(true);
+        else
+            agregar.setEnabled(false);
+
+
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent,RespuestaIMAGEN);
+            }
+        });
+
+    }
+
+
+    private boolean mayRequestStoragePermission() {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return true;
+
+        if ((checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED))
+            return true;
+
+        if ((shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE))) {
+            Snackbar.make(layout, "Los permisos son necesarios para poder usar la aplicación",
+                    Snackbar.LENGTH_INDEFINITE).setAction(android.R.string.ok, new View.OnClickListener() {
+                @TargetApi(Build.VERSION_CODES.M)
+                @Override
+                public void onClick(View v) {
+                    requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS);
+                }
+            });
+        } else {
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS);
+        }
+
+        return false;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case android.R.id.home:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_PERMISSIONS) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permisos aceptados", Toast.LENGTH_SHORT).show();
+                agregar.setEnabled(true);
+            }
+        } else {
+            Toast.makeText(this, "pailas cucu", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        if (requestCode == RespuestaIMAGEN && resultCode == RESULT_OK) {
+
+            mProgressDialog.setMessage("Subiendo...");
+            mProgressDialog.show();
+
+            Uri uri = data.getData();
+            image.setImageURI(uri);
+            StorageReference filepath = mStorageRef.child("parciales").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    mProgressDialog.dismiss();
+                    Toast.makeText(getApplicationContext(), "archivo subido", Toast.LENGTH_LONG).show();
+                    // Get a URL to the uploaded content
+                    //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                }
+            });
+        }
+
+
+    }
 }
+
+
